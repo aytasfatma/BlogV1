388b6539a266259d76475861281f4fbf8c7a0a98
Fatma Aytaş <163586264+aytasfatma@users.noreply.github.com>
2026-03-18 17:29:51 +0300

    Fix project paths and layout design, clean up unnecessary files

--- obj/Release/net8.0/BlogV1.AssemblyInfo.cs
+++ obj/Release/net8.0/BlogV1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogV1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a52fa1fec4a7e059ef430b3289c5060e04b91728")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogV1")]
